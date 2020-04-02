255b42352c10dc60b29b1ba6ef35c5adf50feb3d
lufy90 <very90@foxmail.com>
2020-04-02 10:40:53 +0800

    first stage, barely could be used.

---
+++ specjvm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# filename: specjvm.sh
+# Author: lufei
+# Date: 20200107 15:27:36
+
+SITE=https://www.spec.org/jvm2008/
---
+++ stream.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# filename: stream.sh
+# Author: lufei
+# Date: 20200107 14:12:43
+
+
+SITE=http://www.cs.virginia.edu/stream/ref.html
+
