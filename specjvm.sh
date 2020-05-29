2eb23b505594cb9f19264fc2435ff1afa5970786
lufy90 <very90@foxmail.com>
2020-05-29 14:13:47 +0800

    delete useless, update runtool script

--- specjvm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# filename: specjvm.sh
-# Author: lufei
-# Date: 20200107 15:27:36
-
-SITE=https://www.spec.org/jvm2008/
--- stream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# filename: stream.sh
-# Author: lufei
-# Date: 20200107 14:12:43
-
-
-SITE=http://www.cs.virginia.edu/stream/ref.html
-
